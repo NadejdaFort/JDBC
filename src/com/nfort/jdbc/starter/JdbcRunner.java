3d41a2620cd2db04aae7aaf141d01eead30948aa
Nadejda Doborina <NadejdaFort@gmail.com>
2022-01-28 20:25:13 +0300

    JDBC_5

--- src/com/nfort/jdbc/starter/JdbcRunner.java
+++ src/com/nfort/jdbc/starter/JdbcRunner.java
@@ -5,14 +5,27 @@ import org.postgresql.Driver;
 
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class JdbcRunner {
 
     public static void main(String[] args) throws SQLException {
        // Class<Driver> driverClass = Driver.class;
+        String sql;
+        sql = """
+               CREATE TABLE IF NOT EXISTS info 
+               (
+               id SERIAL PRIMARY KEY,
+               data TEXT NOT NULL 
+               );                
+                """;
 
-        try (var connection = ConnectionManager.open()) {
+        try (var connection = ConnectionManager.open();
+             var statement = connection.createStatement()) {
+            System.out.println(connection.getSchema());
             System.out.println(connection.getTransactionIsolation());
+            var executeResult = statement.execute(sql);
+            System.out.println(executeResult);
         }
     }
 }
