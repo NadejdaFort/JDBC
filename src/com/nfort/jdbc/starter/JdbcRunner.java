6fe05c69de2c28d230dec28f726e3efb432b0991
Nadejda Doborina <NadejdaFort@gmail.com>
2022-01-31 21:10:03 +0300

    JDBC_7

--- src/com/nfort/jdbc/starter/JdbcRunner.java
+++ src/com/nfort/jdbc/starter/JdbcRunner.java
@@ -4,6 +4,7 @@ import com.nfort.jdbc.starter.util.ConnectionManager;
 import org.postgresql.Driver;
 
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -13,21 +14,27 @@ public class JdbcRunner {
        // Class<Driver> driverClass = Driver.class;
         String sql;
         sql = """
-               UPDATE info
-               SET data = 'TestTest'
-               WHERE id = 5
-               RETURNING *;              
-                """;   // в этом запросе возвращается ResultSet, а не как ожидается - int,
-                        // это происходит из-за RETURNING, без него вернется int
+               SELECT *
+               FROM ticket;              
+               """;
 
         try (var connection = ConnectionManager.open();
-             var statement = connection.createStatement()) {
+             var statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE)) {
             System.out.println(connection.getSchema());
             System.out.println(connection.getTransactionIsolation());
 //            var executeResult = statement.execute(sql);
 //            System.out.println(statement.getUpdateCount());
-            var executeResult = statement.executeUpdate(sql);
-            System.out.println(executeResult);
+
+//            var executeResult = statement.executeUpdate(sql);
+            var executeResult = statement.executeQuery(sql);
+            while(executeResult.next()) {
+                System.out.println(executeResult.getLong("id"));
+                System.out.println(executeResult.getString("passenger_no"));
+                System.out.println(executeResult.getBigDecimal("cost"));
+//                executeResult.updateLong("id", 1000);      // обновляет значение у текущей строки
+//                executeResult.last();
+                System.out.println("-----------------------");
+            }
         }
     }
 }
